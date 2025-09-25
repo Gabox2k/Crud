bc0fa2485c87b1472a0286d433e2dacaab9701e7
Gabriel Raidan <sforza1383@gmail,com>
2025-09-25 14:37:41 -0300

    se agrego archivo el paquete y el servidor

---
+++ sever.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const articuloRoutes = require('./routes/articuloRoutes')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
+
+const app = express()
+const PORT = 3000
+
+app.set('view engine' , 'ejs')
+app.set ('views' , path.join(__dirname,'views'))
+
+app.use('/articulos' , articuloRoutes)
+app.listen(PORT, () =>{
+    console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
